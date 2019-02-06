ca7f94faf50ca4416b3dace1c0e992a888d1070d
fu <francescourbanidue@gmail.com>
2019-02-06 09:49:45 +0100

    moved ui into ui/

--- src/syRF_ui.py
+++ src/syRF_ui.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'syRF_ui.ui'
+# Form implementation generated from reading ui file '../ui/syRF.ui'
 #
 # Created by: PyQt5 UI code generator 5.10.1
 #
