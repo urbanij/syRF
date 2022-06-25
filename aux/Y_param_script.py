72443713d94533ce87f26ca11336efbb3bd13731
Francesco Urbani <francescourbanidue@gmail.com>
2022-06-25 16:57:33 +0200

    black formatting

--- aux/Y_param_script.py
+++ aux/Y_param_script.py
@@ -23,46 +23,57 @@ y_L = complex(input(">>> y_L = "))
 
 
 Y = np.matrix([[y_i, y_r], [y_f, y_o]])
-print ()
+print()
 # print ("Y = ")
 # print (Y)
-print ()
+print()
 
 
-
-
-C            = ( abs(y_r*y_f) ) / ( 2*y_i.real*y_o.real - (y_r*y_f).real )
-beta_A       = (y_f*y_r)/( (y_i+y_s)*(y_o+y_L) )
+C = (abs(y_r * y_f)) / (2 * y_i.real * y_o.real - (y_r * y_f).real)
+beta_A = (y_f * y_r) / ((y_i + y_s) * (y_o + y_L))
 beta_A_polar = cmath.polar(beta_A)
-y_in         = y_i - (y_r*y_f)/(y_o + y_L)
-y_out        = y_o - (y_f*y_r)/(y_i+y_s)
-G_A          = ( abs(y_f)**2 * y_s.real ) / ( (y_o*y_s + y_o*y_i - y_r*y_f)*(y_i+y_s).conjugate() ).real
-G_P          = ( (abs(y_f)**2) / (abs(y_o+y_L)**2) ) * (y_L.real)/(y_in.real)
-G_T          = ( 4*y_s.real*y_L.real*abs(y_f)**2)/abs((y_s+y_i)*(y_o+y_L)-y_r*y_f)**2
-k            = ( 2*(y_i.real+y_s.real)*(y_o.real+y_L.real) )/ ( (y_r*y_f).real + abs(y_r*y_f) )
+y_in = y_i - (y_r * y_f) / (y_o + y_L)
+y_out = y_o - (y_f * y_r) / (y_i + y_s)
+G_A = (abs(y_f) ** 2 * y_s.real) / (
+    (y_o * y_s + y_o * y_i - y_r * y_f) * (y_i + y_s).conjugate()
+).real
+G_P = ((abs(y_f) ** 2) / (abs(y_o + y_L) ** 2)) * (y_L.real) / (y_in.real)
+G_T = (4 * y_s.real * y_L.real * abs(y_f) ** 2) / abs(
+    (y_s + y_i) * (y_o + y_L) - y_r * y_f
+) ** 2
+k = (2 * (y_i.real + y_s.real) * (y_o.real + y_L.real)) / (
+    (y_r * y_f).real + abs(y_r * y_f)
+)
 
-g_s_opt      = ((2*y_i.real*y_o.real - (y_r*y_f).real)**2 - abs(y_r*y_f)**2)**0.5/(2*y_o.real)
-b_s_opt      = -y_i.imag + (y_r*y_f).imag/(2*y_o.real)
-y_s_opt      = g_s_opt + b_s_opt*1j
+g_s_opt = (
+    (2 * y_i.real * y_o.real - (y_r * y_f).real) ** 2 - abs(y_r * y_f) ** 2
+) ** 0.5 / (2 * y_o.real)
+b_s_opt = -y_i.imag + (y_r * y_f).imag / (2 * y_o.real)
+y_s_opt = g_s_opt + b_s_opt * 1j
 
-g_L_opt      = (g_s_opt*y_o.real)/(y_i.real)
-b_L_opt      = -y_o.imag + (y_r*y_f).imag/(2*y_i.real)
-y_L_opt      = g_L_opt + b_L_opt*1j
-
-
-
-print ("C       = " + str(C))
-print ("beta*A  = " + str(beta_A) + " = " + str(beta_A_polar[0]) + "∠" + str(beta_A_polar[1]) + " rad")
-print ("y_in    = " + str(y_in))
-print ("y_out   = " + str(y_out))
-print ("G_A     = " + str(G_A))
-print ("G_P     = " + str(G_P))
-print ("G_T     = " + str(G_T))
-print ("k       = " + str(k))
-print ("y_s_opt = " + str(y_s_opt))
-print ("y_L_opt = " + str(y_L_opt))
-print ()
-print ()
-print ()
+g_L_opt = (g_s_opt * y_o.real) / (y_i.real)
+b_L_opt = -y_o.imag + (y_r * y_f).imag / (2 * y_i.real)
+y_L_opt = g_L_opt + b_L_opt * 1j
 
 
+print("C       = " + str(C))
+print(
+    "beta*A  = "
+    + str(beta_A)
+    + " = "
+    + str(beta_A_polar[0])
+    + "∠"
+    + str(beta_A_polar[1])
+    + " rad"
+)
+print("y_in    = " + str(y_in))
+print("y_out   = " + str(y_out))
+print("G_A     = " + str(G_A))
+print("G_P     = " + str(G_P))
+print("G_T     = " + str(G_T))
+print("k       = " + str(k))
+print("y_s_opt = " + str(y_s_opt))
+print("y_L_opt = " + str(y_L_opt))
+print()
+print()
+print()
--- aux/constant_resistance_intersection.py
+++ aux/constant_resistance_intersection.py
@@ -17,23 +17,25 @@ import cmath
 
 #### copied from intersection file. import this instead!
 
+
 def find_intersection_points(c1, r1, c2, r2):
     # c1 and c2 are the coordinates of the center (complex number) and r1 r2 their radii
 
-    d = math.hypot(c2.real - c1.real, c2.imag - c1.imag)    # hypot: return the Euclidean norm, sqrt(x*x + y*y). 
-                                                            # This is the length of the vector from the origin 
-                                                            # to point (x, y).
+    d = math.hypot(
+        c2.real - c1.real, c2.imag - c1.imag
+    )  # hypot: return the Euclidean norm, sqrt(x*x + y*y).
+    # This is the length of the vector from the origin
+    # to point (x, y).
     if d <= r1 + r2 and d >= abs(r2 - r1):
         ex = (c2.real - c1.real) / d
         ey = (c2.imag - c1.imag) / d
-        x  = (r1**2 - r2**2 + d**2) / (2*d)
-        y  = (r1**2 - x**2)**0.5
-    
-        P1 = c1.real + x*ex + y*ey + (c1.imag + x*ey - y*ex)*1j
-        P2 = c1.real + x*ex - y*ey + (c1.imag + x*ey + y*ex)*1j
-        
-        return P1, P2
+        x = (r1**2 - r2**2 + d**2) / (2 * d)
+        y = (r1**2 - x**2) ** 0.5
 
+        P1 = c1.real + x * ex + y * ey + (c1.imag + x * ey - y * ex) * 1j
+        P2 = c1.real + x * ex - y * ey + (c1.imag + x * ey + y * ex) * 1j
+
+        return P1, P2
 
 
 c1 = 0.5
@@ -47,6 +49,5 @@ gamma_points = find_intersection_points(c1, r1, c2, r2)
 print(gamma_points)
 
 for gamma_point in gamma_points:
-    z = (1+gamma_point)/(1-gamma_point)
+    z = (1 + gamma_point) / (1 - gamma_point)
     print(f"{z:.4}")
-
